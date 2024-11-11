969a25ca2a323f7c00a65cb3855634e7ade37d15
shpd <sleepyawn@yandex.ru>
2024-11-11 12:37:35 +0300

    README

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('guests', GuestController::class);
 
-
-// routes/api.php
-
 Route::any('/{any}', function () {
     return response()->json(['error' => 'error route'], 404);
 })->where('any', '.*');
