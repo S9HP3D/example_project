50ba313c957f1f4fbc92d09f228da2062393baf3
shpd <sleepyawn@yandex.ru>
2024-11-11 11:02:40 +0300

    api route error_response

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,10 @@ use App\Http\Api\GuestController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('guests', GuestController::class);
+
+
+// routes/api.php
+
+Route::any('/{any}', function () {
+    return response()->json(['error' => 'error route'], 404);
+})->where('any', '.*');
